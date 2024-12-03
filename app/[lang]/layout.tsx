7df084b3b14850383fd52ee1c4be1acb1c64b52b
Lorenz Boss <lorenz.boss@bbzbl-it.ch>
2024-12-03 23:57:32 +0100

    Initial commit.

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import Link from "next/link";
+import "../globals.css";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "../fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "../fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
@@ -28,6 +29,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header>
+          <div className="space-x-2">
+            <Link href="/en">English</Link>
+            <span>|</span>
+            <Link href="/de">Deutsch</Link>
+          </div>
+        </header>
         {children}
       </body>
     </html>
